aadc61836486d58478a475ea517fff53cba72bda
suryadev <gouru.surya@gmail.com>
2021-01-07 21:00:10 +0530

    let const var

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,15 +1,22 @@
 //VALUES AND VARIABLES
-let country = 'India';
-let continent = 'Asia';
-let population = 10;
-console.log(country);
-console.log(continent);
-console.log(population);
+// let country = 'India';
+// let continent = 'Asia';
+// let population = 10;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 //DATA TYPES
-let isIsland = false;
-let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+//LET CONST VAR
+language = 'telugu';
+const country = 'India';
+const continent = 'Asia';
+const isIsland = false;
+isIsland = true;
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -17,7 +17,6 @@ let myCurrentJob = 'Teacher';
 
 let job1 = 'programmer';
 let job2 = 'Teacher';
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -37,5 +36,26 @@ console.log(typeof year);
 year = 1991;
 console.log(typeof year);
 
-console.log(typeof null);
+console.log(typeof null); //object which is a bug in javascript
+*/
+
+let age = 30;
+age = 31; //mutated a variable
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//const job;
+
+//let is block scoped
+//var is function scoped
+
+var job = 'programmer';
+job = 'teacher';
+
+//this will not create variable in current local scope
+//rather it creates a property on the global object
+lastName = 'gouru';
+console.log(lastName);
+
 
