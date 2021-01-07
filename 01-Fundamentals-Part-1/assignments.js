9c9b0ea59c231f1a77a1578dc055b03d7abc87d1
suryadev <gouru.surya@gmail.com>
2021-01-07 20:36:40 +0530

    values and variables

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,6 @@
+let country = 'India';
+let continent = 'Asia';
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,19 @@
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
-40+8+22-10;
 console.log(40+8+22-10);
+
+console.log("SuryaDev");
+console.log(23);
+
+let firstName = "SuryaDev";
+console.log(firstName);
+
+//$ and _ are only special chars allowed in variable naming
+let person = 'SuryaDev';
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'Teacher';
+
