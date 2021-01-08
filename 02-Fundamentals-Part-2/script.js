6f86852a5936b26495d0d1630452281b6ea5aaad
suryadev <gouru.surya@gmail.com>
2021-01-08 21:19:53 +0530

    functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -3,6 +3,8 @@
 //2. It creates visible errors for us without which js fails silently
 'use strict';
 
+/*
+
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,3 +13,29 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 const interface = 'Audio'; 
 const private = 534; //strict mode reserves these words for the future
+
+*/
+
+function logger() {
+    console.log('My name is suryadev');
+}
+
+//invoking / running / calling function
+logger(); 
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
