5a07cbeddc1f2960c3a5bdbe79901f1da2937e2d
suryadev <gouru.surya@gmail.com>
2021-01-08 11:21:36 +0530

    logical operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -235,7 +235,7 @@ if (height) {
     console.log('Height is UNDEFINED');
 }
 
-*/
+
 
 const age = 18; //'18'
 if (age === 18) console.log('You just became an adult :D (strict)');
@@ -262,3 +262,47 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log('Why not 23?');
+
+const hasDriverLicense = true; //A
+const hasGoodVision = true; //B
+
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const isTired = true;
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+    console.log('Bhoomi is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+*/
+
+// const scoreDolphins = (96 + 108 + 69) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy 🏆'); 
+// } else if (scoreKoalas > scoreDolphins) {
+//     console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy!');
+// }
+
+//BONUS 1
+
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy 🏆'); 
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy 🏆');
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!');
+} else {
+    console.log('No one wins the trophy');
+}
