5a28fad081a2b35388958c33292af4484a100216
suryadev <gouru.surya@gmail.com>
2021-01-08 22:36:47 +0530

    function decl and stmt

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,7 +14,6 @@ if (hasDriversLicense) console.log('I can drive :D');
 const interface = 'Audio'; 
 const private = 534; //strict mode reserves these words for the future
 
-*/
 
 function logger() {
     console.log('My name is suryadev');
@@ -38,4 +37,34 @@ const appleOrangeJuice = fruitProcessor(2,4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+*/
+
+//Function declarations can be called before the declaration
+//This happens because of process called hoisting
+const age1 = calcAge1(1996);
+console.log(age1);
+
+//Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+//Function expression;
+//Anonymous function
+// function is a value and not a type
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1996);
+
+console.log(age1, age2);
+
+
+
+
+
+
+
+
 
