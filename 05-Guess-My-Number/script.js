a5218f814bd95833f649a61cea81d77aecafd7be
suryadev <gouru.surya@gmail.com>
2021-01-09 20:15:37 +0530

    dom manipulation

---
+++ 05-Guess-My-Number/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//querySelector is method which is available on the document object
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
