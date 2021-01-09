06acf1d4a711c12fa01310e3c127cc36d5575b3a
suryadev <gouru.surya@gmail.com>
2021-01-09 20:29:16 +0530

    handling click events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //querySelector is method which is available on the document object
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
 
@@ -9,3 +10,21 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+//function is also just a value in javascript ; this is also called an event handler
+//we are attaching the click event handler on the check button
+const x = function () {
+  console.log(23);
+};
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); //empty value gets converted to 0
+  console.log(guess, typeof guess);
+  //guess is falsy
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️No number!';
+  }
+});
+
+//we are just defining the function and we are not calling it
+//Javascript engine calls the function as soon as event happens on the button
