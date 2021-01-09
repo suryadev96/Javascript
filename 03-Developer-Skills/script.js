8c200986ead4ac7d94f699418978eb1f69498a59
suryadev <gouru.surya@gmail.com>
2021-01-09 13:25:10 +0530

    debug with console and bp

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,7 +1,90 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 const x = '23';
 if (x === 23) console.log(23);
 
 const calcAge = birthYear => 2037 - birthYear;
+console.log(calcAge(1996));
+
+
+//PROBLEM:
+// We work for a company building a smart home thermometer.
+// Our most recent task is this: "Given an array of temperatures of one day , calculate the temperature amplitude"
+//Keep in mind there might be sensor error
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAltitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAltitude([3, 7, 4]);
+console.log(amplitude);
+
+//PROBLEM 2: INcludes merging 2 arrays
+const calcTempAltitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAltitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    //value: Number(prompt('Degrees celsius;')),
+    value: 10,
+  };
+
+  console.log(measurement);
+  console.table(measurement);
+
+  console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+console.log(measureKelvin());
+
+//using a debugger
+const calcTempAltitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    //debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAltitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
