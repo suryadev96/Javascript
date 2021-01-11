18a225c46d91b0d88d4132fabb85de0ab90eaf94
suryadev <gouru.surya@gmail.com>
2021-01-11 21:25:44 +0530

    enhanced object literals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,50 +1,59 @@
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+//COMPUTING PROPERTY NAMES
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, //Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, //Open 24 hours
-      close: 24,
-    },
-  },
+  //PRE ES6
+  //openingHours: openingHours,
+  openingHours,
 
-  order: function (starterIndex, mainIndex) {
+  //new way of writing function methods
+
+  //   order: function (starterIndex, mainIndex) {
+  //     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  //   },
+
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
       will be delivered to ${address} at ${time}`
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+/*
 ////LOOPING ARRAYS: FOR-OF LOOP
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -60,6 +69,7 @@ for (const [i, el] of menu.entries()) {
 
 console.log(menu.entries());
 console.log(...menu.entries());
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #1
