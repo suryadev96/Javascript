8a4cff6f75036ccb8b5383d05d4b0584d6976ef4
suryadev <gouru.surya@gmail.com>
2021-01-14 20:32:55 +0530

    filter method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -255,3 +255,20 @@ const movementsDescription = movements.map(
 console.log(movementsDescription);
 
 */
+
+////Filter method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const depositsFor;
+for (const mov of movements) {
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+}
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
