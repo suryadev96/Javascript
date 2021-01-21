ea766d1d84ad85589182322424e5670a08a72fef
suryadev <gouru.surya@gmail.com>
2021-01-21 19:05:08 +0530

    types of events and handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -151,3 +151,21 @@ btnScrollTo.addEventListener('click', function (e) {
   //Modern way
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! you are reading the heading :D');
+  //h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! you are reading the heading :D');
+// };
+
+//addevent listeners ; we can attach multiple event handlers
+//whereas onEvent overrides the previous handlers
