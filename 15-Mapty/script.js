e5a83a71b6551cf3b6cc80dbd58d726696f4a7cd
suryadev <gouru.surya@gmail.com>
2021-01-23 22:06:54 +0530

    planning project

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -22,3 +22,25 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+/*
+////////////////////
+//// User Stories
+1.As a user, i want to log my running workouts with location, distance, time, pace and steps/minute so i can keep a log of all my running
+2.As a user, i want to log my cycling workouts with location, distance, time, speed and elevation so i can keep a log of all my cycling
+3.As a user, i want to see all my workouts at a glance, so i can easily track my progress over time
+4.As a user, i want to also see my workouts on a map, so i can easily check where i workout the most
+5.As a user, i want to see all my workouts when i leave the app and come back later, so that i can keep using the app over time
+
+//////////////////
+1.user story features
+a. Map where user clicks to add new workout (best way to get location coords)
+b. Geolocation to display map at current location (more user friendly)
+c. Form to input distance, time, pace, steps/minute
+
+3. Display all workouts in a list
+
+4. Display all workouts in a map
+
+5. Store workout data in the browser using local storage API. On page load, read the saved data from local storage and display
+*/
