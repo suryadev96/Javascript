3597e8e45a4eaad40a81212e80095db0f1ead9e2
suryadev <gouru.surya@gmail.com>
2021-01-23 12:36:26 +0530

    Object.create prototype

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -207,3 +207,28 @@ console.log(Array.from(document.querySelectorAll('h1')));
 //console.log([1, 2, 3].from());
 
 console.log(Number.parseFloat(12));
+
+//Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1997);
+sarah.calcAge();
