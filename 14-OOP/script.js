747cd3f39cffef517803d65c26e4e8def7bae531
suryadev <gouru.surya@gmail.com>
2021-01-23 14:28:12 +0530

    account class example

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -338,6 +338,7 @@ DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 
 GOOD LUCK 😀
 */
+/*
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -470,3 +471,52 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+//////////////////////
+//Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  //public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved!`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+//acc1.approveLoan(1000); //should not be used; data encapsulation and data privacy
+
+console.log(acc1);
+console.log(acc1.pin);
