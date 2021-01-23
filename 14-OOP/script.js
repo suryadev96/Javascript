b206109b6109dcafc99223ab7f71f9e73a2a98ba
suryadev <gouru.surya@gmail.com>
2021-01-23 14:11:55 +0530

    inheritance : ES6 classes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -377,3 +377,67 @@ const telsa = new EV('Tesla', 120, 23);
 telsa.chargeBattery(90);
 telsa.brake();
 telsa.accelerate();
+
+//////////////////////////////
+// Inheritance Between "Classes": ES6 classes
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  //Instance methods
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name !`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  //static method
+  static hey() {
+    console.log('Hey there 👋');
+  }
+}
+
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    //Always needs to happen first;
+    //super call is responsible for creating this keyword in the subclass
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+martha.introduce();
+martha.calcAge();
