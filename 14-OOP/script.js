d9d32b8288bbcc22ca42963886abfe3bd383250e
suryadev <gouru.surya@gmail.com>
2021-01-23 10:03:41 +0530

    constructor & Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  //Instance Properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Never do this; as we would be creating so many copies of this function when we instantiate;
+  //we would have attached to each and every single object instantiated using this constructor function
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const surya = new Person('suryadev', 1996);
+console.log(surya);
+
+//All the below steps happens because we are calling the function with new keyword
+//1. New empty object {} is created
+//2. function is called, this keyword is set to new empty object {}
+//3. {} is linked to Prototype
+//4. function automatically return {}; need not be empty
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+//We did created the objects from the constructor function
+//Constructor function are simulations to the classes
+
+console.log(surya instanceof Person);
+//console.log(jay instanceof Person);
+
+//Prototypes
+console.log(Person.prototype);
+
+//First each and every function including constructor functions in javascript actually has a property called prototype
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+surya.calcAge(); //this keyword is set to the object calling this method
+matilda.calcAge();
+
+console.log(surya.__proto__);
+console.log(surya.__proto__ === Person.prototype); //true
+
+console.log(Person.prototype.isPrototypeOf(surya));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(surya, matilda);
+console.log(surya.species, matilda.species);
+
+console.log(surya.hasOwnProperty('firstName'));
+console.log(surya.hasOwnProperty('species'));
